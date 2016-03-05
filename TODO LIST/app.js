402cbcf05bb593be2bb09cdd9d9efdcc1e578418
lamouche <lamouche444@hotmail.fr>
2016-03-05 15:43:36 +0100

    cours

---
+++ TODO LIST/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lamou on 05/03/2016.
+ *
+ *  - express / socket.io
+ *  - EJS (systeme de template)
+ *  - Les routes (Lister les taches, Ajouter une tâche, supprimer une tâche)
+ *
+ *
+ *
+ *
+ **/
